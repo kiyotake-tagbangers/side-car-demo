c21c26d9fe1d1080d948f0092e1f096a73b5c8cd
kiyota takeshi <kiyota@tagbangers.co.jp>
2020-11-20 20:58:52 +0900

    add loggin

--- src/main/java/com/kiyotagbangers/demo/controller/HelloWorldController.java
+++ src/main/java/com/kiyotagbangers/demo/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.kiyotagbangers.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HelloWorldController {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping
-    public String hello(){
+    public String hello() {
+        logger.info("side-car-spring");
         return "Hello World";
     }
 }
